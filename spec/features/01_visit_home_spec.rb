fd9d19c49c336f0d5fb19c87d25582e47d0584d6
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-03-17 23:11:24 -0300

    verify links exists

--- spec/features/01_visit_home_spec.rb
+++ spec/features/01_visit_home_spec.rb
@@ -13,4 +13,13 @@ feature 'Visit The-Internet Home Page' do
   scenario 'Current URL: https://madmusic9.herokuapp.com/' do
     expect(page).to have_current_path('https://madmusic9.herokuapp.com/')
   end
+
+  scenario 'Verify Link Exists: Sign Up' do
+    expect(page).to have_link('SIGN-UP')
+  end
+
+  scenario 'Click Link: Sign Up ==> /users/sign_up' do
+    click_link('SIGN-UP')
+    expect(page).to have_current_path('/users/sign_up')
+  end
 end
