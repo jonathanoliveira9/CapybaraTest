afaa9361ed688b344139a7b7315c8879b23bfbe3
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-03-17 22:28:49 -0300

    configure spec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require 'bundler'
+require 'capybara/dsl'
+require 'capybara/rspec'
+
+Bundler.setup(:default)
+Bundler.require
+
+Capybara.default_driver = :selenium_chrome
+Capybara.app_host = 'https://madmusic9.herokuapp.com/'
+Capybara.default_max_wait_time = 10
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
