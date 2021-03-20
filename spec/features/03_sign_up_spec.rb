c8852ebbfcd99d2348802492aaafd54035341dd8
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-03-20 14:18:32 -0300

    valid email has already been taken

--- spec/features/03_sign_up_spec.rb
+++ spec/features/03_sign_up_spec.rb
@@ -14,5 +14,15 @@ feature 'Sign-UP User: Validate Page Elements' do
     click_button('Sign up')
     expect(page).to have_css("li", text: "Password confirmation doesn't match Password")
   end
+
+  scenario 'Email has already been taken' do
+    password = Faker::Internet.password(min_length: 10, max_length: 20, mix_case: true, special_characters: true)
+    fill_in('Name', with: Faker::Book.author)
+    fill_in('Email', with: 'jonathan@test.com')
+    fill_in('Password', with: password)
+    fill_in('Password confirmation', with: password)
+    click_button('Sign up')
+    expect(page).to have_css("li", text: "Email has already been taken")
+  end
 end
 
