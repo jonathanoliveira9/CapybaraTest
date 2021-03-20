f6233cd9b7cbb5b3df8845d3d9f48e583790c1b2
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-03-20 15:11:55 -0300

    success to sign up

--- spec/features/03_sign_up_spec.rb
+++ spec/features/03_sign_up_spec.rb
@@ -24,5 +24,15 @@ feature 'Sign-UP User: Validate Page Elements' do
     click_button('Sign up')
     expect(page).to have_css("li", text: "Email has already been taken")
   end
+
+  scenario 'Successfully to Sign Up' do
+    password = Faker::Internet.password(min_length: 10, max_length: 20, mix_case: true, special_characters: true)
+    fill_in('Name', with: Faker::Book.author)
+    fill_in('Email', with: Faker::Internet.email(name: 'Thor')) 
+    fill_in('Password', with: password)
+    fill_in('Password confirmation', with: password)
+    click_button('Sign up')
+    expect(page).to have_content('Welcome! You have signed up successfully.')
+  end
 end
 
