b93ee7a69aba5b49ff72c47a6b3dd70821f37019
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-03-28 16:34:36 -0300

    create a new branch

--- spec/features/04_create_branch_spec.rb
+++ spec/features/04_create_branch_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 require_relative '../support/helpers/login_helper'
-
+  
 feature 'Register Brand' do
   include Helpers::LoginHelper
 
@@ -13,5 +13,15 @@ feature 'Register Brand' do
     click_on('BRANDS')
     click_on('Novo')
     expect(page).to have_content('Register Brand')
+  end
+
+  scenario 'Register Brand Modal: Success to Create' do
+    sign_in_as('jonathanoliveira@test.com', '123456')
+    click_on('BRANDS')
+    click_on('Novo')
+    fill_in('brand[fantasy_name]', with: Faker::Game.title)
+    fill_in('brand[description]', with: Faker::Lorem.paragraph)
+    click_on('New')
+    expect(page).to have_content('Brand was successfully created')
   end
 end
